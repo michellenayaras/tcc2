2e1bc977a4d6e3f5bcdb121150f51eda0d38e311
michellenayaras <michellenayara94@gmail.com>
2023-03-10 19:21:29 -0300

    att

---
+++ lib/app/views/secrecoes/estudo_secrecoes.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+
+class EstudoSecrecoes extends StatelessWidget {
+  const EstudoSecrecoes({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(
+            255, 250, 249, 249), // Define a cor de fundo do AppBar
+        leading: IconButton(
+          // Define o botão de retorno com um ícone personalizado
+          icon: const Icon(
+            Icons.arrow_back_ios_rounded,
+            color: Color.fromRGBO(153, 184, 196, 100),
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: const Text(
+          'Estudo - Secreções',
+          style: TextStyle(
+            color: Color.fromRGBO(62, 132, 158, 100),
+            fontSize: 20.0,
+            fontFamily: 'Roboto',
+          ),
+        ),
+        centerTitle: true,
+        toolbarHeight: 70.0,
+        elevation: 2.0,
+      ),
+       body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+        child: ListView(
+          children: <Widget>[
+            const SizedBox(height: 8),
+            ListTile(
+              title: const Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 16.0,
+                ),
+                child: Text(
+                  'Tema 1',
+                  style: TextStyle(
+                    color: Color.fromARGB(156, 31, 99, 124),
+                    fontSize: 16.0,
+                    fontFamily: 'Roboto',
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+              onTap: () {
+                // Navegar para a tela do tema 1
+              },
+              tileColor: Colors.white, // Define a cor de fundo do item
+              contentPadding: const EdgeInsets.symmetric(vertical: 8.0),
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                    Radius.circular(8.0)), // Define o raio da borda do item
+                side: BorderSide(
+                  color: Colors.grey,
+                  width: 0.5,
+                ),
+              ),
+            ),
+            const SizedBox(height: 5),
+            ListTile(
+              title: const Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 16.0,
+                ),
+                child: Text(
+                  'Tema 2',
+                  style: TextStyle(
+                    color: Color.fromARGB(156, 31, 99, 124),
+                    fontSize: 16.0,
+                    fontFamily: 'Roboto',
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+              onTap: () {
+                // Navegar para a tela do tema 2
+              },
+              tileColor: Colors.white,
+              contentPadding: const EdgeInsets.symmetric(vertical: 8.0),
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                side: BorderSide(
+                  color: Colors.grey,
+                  width: 0.5,
+                ),
+              ),
+            ),
+            const SizedBox(height: 5),
+            ListTile(
+              title: const Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 16.0,
+                ),
+                child: Text(
+                  'Tema 3',
+                  style: TextStyle(
+                    color: Color.fromARGB(156, 31, 99, 124),
+                    fontSize: 16.0,
+                    fontFamily: 'Roboto',
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+              onTap: () {
+                // Navegar para a tela do tema 2
+              },
+              tileColor: Colors.white,
+              contentPadding: const EdgeInsets.symmetric(vertical: 8.0),
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                side: BorderSide(
+                  color: Colors.grey,
+                  width: 0.5,
+                ),
+              ),
+            ),
+            const SizedBox(height: 5),
+            ListTile(
+              title: const Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 16.0,
+                ),
+                child: Text(
+                  'Tema 4',
+                  style: TextStyle(
+                    color: Color.fromARGB(156, 31, 99, 124),
+                    fontSize: 16.0,
+                    fontFamily: 'Roboto',
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+              onTap: () {
+                // Navegar para a tela do tema 2
+              },
+              tileColor: Colors.white,
+              contentPadding: const EdgeInsets.symmetric(vertical: 8.0),
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                side: BorderSide(
+                  color: Colors.grey,
+                  width: 0.5,
+                ),
+              ),
+            ),
+            // Adicione mais itens da lista aqui
+          ],
+        ),
+      ),
+    );
+  }
+}
